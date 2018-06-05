47a203ce7597e8bedfdc20824f1172005c2d0f9a
Brendan Draper <brendanmdraper@gmail.com>
2018-06-05 11:07:11 -0400

    .

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { LocalStorageService } from '../shared/services/local-storage.service';
 
 @Component({
   selector: 'app-header',
@@ -9,10 +10,23 @@ import { Router } from '@angular/router';
 export class HeaderComponent implements OnInit {
 
   constructor(
-    private router: Router
+    private router: Router,
+    private ls: LocalStorageService
   ) { }
 
+  user;
+
   ngOnInit() {
+    // user a custom localstorage service to subscribe to an observable for change detection
+    //TODO: when logging out, component is changed after it has been checked, which throws an error
+    this.user = JSON.parse(localStorage.getItem('currentUser'));
+    this.ls.detectChanges().subscribe(change => {
+      if (change === 'set') {
+        this.user = JSON.parse(localStorage.getItem('currentUser'));
+      } else if (change === 'removed') {
+        this.user = null;
+      }
+    });
   }
 
   goHome() {
